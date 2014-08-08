d1a4a8cc2d7157551d23354abda9ee79fc977aed
igor.kostromin <elwood.su@gmail.com>
2014-08-08 23:57:44 +0400

    * node-edit.zul extracted.

--- src/main/scala/ru/MainVm.scala
+++ src/main/scala/ru/MainVm.scala
@@ -4,7 +4,8 @@ import java.io.File
 import java.util
 
 import com.google.gson.Gson
-import org.zkoss.bind.annotation.{Init, ContextType, ContextParam, Command}
+import org.zkoss.bind.BindUtils
+import org.zkoss.bind.annotation._
 import org.zkoss.zk.ui.select.Selectors
 import org.zkoss.zk.ui.{Component, Executions}
 import org.zkoss.zk.ui.select.annotation.Wire
@@ -17,14 +18,32 @@ import scala.io.{Source, BufferedSource}
  * @author igor.kostromin
  *         08.08.2014 12:04
  */
-class NodeVm(urlPrefix: String, template: String, nodes: Array[Node], attributes: java.util.HashMap[String, Object],
-             modules: Array[ModuleInstance]) extends Node(urlPrefix, template, nodes, attributes, modules) {
-  @BeanProperty var treeItem : Node = null
+class NodeVm {
+  @BeanProperty var node : Node = null
+
+  @Init
+  def init(@BindingParam("node") node: Node) = {
+    System.out.println("Node: " + node.toString)
+    this.node = node
+  }
+
+  def this(node: Node) {
+    this()
+    this.node = node
+  }
 }
 
 class MainVm {
   @BeanProperty var treeVm: TreeVm = null
-  @BeanProperty var currentItemVm: NodeVm = null
+  var currentItemVm: Node = null
+  @BeanProperty var currentItemVmVm: NodeVm = null
+
+  def getCurrentItemVm: Node = currentItemVm
+  def setCurrentItemVm(node: Node): Unit = {
+    currentItemVm = node
+    currentItemVmVm = new NodeVm(node)
+    BindUtils.postNotifyChange(null, null, this, "currentItemVmVm")
+  }
 
   @Wire("#win")
 	var win: Window = null
@@ -44,7 +63,7 @@ class MainVm {
   val rootNode : Node = new Node(null, null, Array(cmsConfig.rootNode), null, null)
   treeVm = new TreeVm(rootNode)
 
-  currentItemVm = new NodeVm("urlPrefix", "template!", null, null, null)
+  //currentItemVm = new Node("urlPrefix", "template!", null, null, null)
 
   @Command(Array("add"))
   def add() {
@@ -54,13 +73,14 @@ class MainVm {
   @Command(Array("edit"))
   def edit() {
     System.out.println("Edit command")
-    val w: Window = new Window("ZK IM - ", "normal", true)
-    w.setPosition("parent")
-    w.setParent(win)
-    val dataArgs = new util.HashMap[String, String]()
-    dataArgs.put("name", "testName")
-    Executions.createComponents("/dialog.zul", w, dataArgs)
-    w.doModal()
+//    val w: Window = new Window("ZK IM - ", "normal", true)
+//    w.setPosition("parent")
+//    w.setParent(win)
+    val dataArgs = new util.HashMap[String, Object]()
+    dataArgs.put("node", currentItemVm)
+    Executions.createComponents("/dialog.zul", win, dataArgs).asInstanceOf[Window]
+    .doModal()
+//    w.doModal()
   }
 
   def canExecute(cmd: String) : Boolean = {
