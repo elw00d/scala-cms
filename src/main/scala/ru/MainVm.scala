a7dbd69f680c3881fc847a10d53cf0414e0f7919
Kostromin Igor <elw00d@users.noreply.github.com>
2014-08-09 18:12:23 +0400

    + Node editing infrastructure.

--- src/main/scala/ru/MainVm.scala
+++ src/main/scala/ru/MainVm.scala
@@ -6,6 +6,7 @@ import java.util
 import com.google.gson.Gson
 import org.zkoss.bind.BindUtils
 import org.zkoss.bind.annotation._
+import org.zkoss.zk.ui.event.{Events, Event, EventListener}
 import org.zkoss.zk.ui.select.Selectors
 import org.zkoss.zk.ui.{Component, Executions}
 import org.zkoss.zk.ui.select.annotation.Wire
@@ -22,8 +23,8 @@ class NodeVm {
   @BeanProperty var node : Node = null
 
   @Init
-  def init(@BindingParam("node") node: Node) = {
-    System.out.println("Node: " + node.toString)
+  def init(@ExecutionArgParam("node") node: Node) = {
+    System.out.println("Node: " + node)
     this.node = node
   }
 
@@ -31,18 +32,24 @@ class NodeVm {
     this()
     this.node = node
   }
+
+  @Command(Array("submit"))
+  def submit() = {
+    System.out.println("NodeVm.submit()")
+  }
 }
 
 class MainVm {
   @BeanProperty var treeVm: TreeVm = null
   var currentItemVm: Node = null
-  @BeanProperty var currentItemVmVm: NodeVm = null
+  @BeanProperty var currentItemVmVm: NodeVm = new NodeVm()
 
   def getCurrentItemVm: Node = currentItemVm
   def setCurrentItemVm(node: Node): Unit = {
     currentItemVm = node
     currentItemVmVm = new NodeVm(node)
     BindUtils.postNotifyChange(null, null, this, "currentItemVmVm")
+    BindUtils.postNotifyChange(null, null, this, "canExecute")
   }
 
   @Wire("#win")
@@ -77,14 +84,49 @@ class MainVm {
 //    w.setPosition("parent")
 //    w.setParent(win)
     val dataArgs = new util.HashMap[String, Object]()
-    dataArgs.put("node", currentItemVm)
-    Executions.createComponents("/dialog.zul", win, dataArgs).asInstanceOf[Window]
-    .doModal()
+    dataArgs.put("nodeVm", currentItemVmVm)
+    val dlg: Window = Executions.createComponents("/dialog.zul", win, dataArgs).asInstanceOf[Window]
+    win.addEventListener("onDlgClosed", new CloseEventListener[Event]())
+    dlg.doModal()
 //    w.doModal()
+  }
+
+  class CloseEventListener[T <: Event] extends EventListener[T] {
+    override def onEvent(event: T): Unit = {
+      //System.out.println("onClosed!!!!!!")
+      // Чтобы в форме справа отразились изменения, сделанные в диалоговом окне
+      BindUtils.postNotifyChange(null, null, MainVm.this, "currentItemVmVm")
+    }
   }
 
   def canExecute(cmd: String) : Boolean = {
     System.out.println(String.format("Can execute (%s)", cmd))
+    if (cmd == "submit")
+      return this.currentItemVm != null
     true
+  }
+
+  @Command(Array("submit"))
+  def submit() = {
+    System.out.println("MainVm.submit()")
+  }
+}
+
+class DialogVm {
+  @Wire("#dlg")
+	var dlg: Window = null
+
+  @Init
+	def init(@ContextParam(ContextType.VIEW) view: Component) {
+		Selectors.wireComponents(view, this, false)
+	}
+
+  @Command(Array("submit"))
+  def submit() = {
+    System.out.println("DialogVm.submit()")
+//    dlg.setVisible(false)
+//    dlg.setId(null)
+    Events.sendEvent("onDlgClosed", dlg.getParent(), null)
+    dlg.detach()
   }
 }
