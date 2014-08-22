47a3e3ea257aa3d023e0c83d95d2d4c6238ebb4c
Kostromin Igor <elw00d@users.noreply.github.com>
2014-08-23 02:05:41 +0400

    + Added working prototype of custom AceEditor control.

--- src/main/resources/web/js/ru/mold/AceEditorComponent.js
+++ src/main/resources/web/js/ru/mold/AceEditorComponent.js
@@ -1,3 +1,22 @@
 function (out) {
-    out.push('<span', this.domAttrs_(), '>', this.getValue(), '</span>');
+    //out.push('<span', this.domAttrs_(), '>', this.getValue(), '</span>');
+
+    out.push('<div id="editor" style="width: 300px; height:200px;" ', this.domAttrs_(), '>', this.getValue(), '</div>');
+
+    this.bind_ = function (desktop, skipper, after) {
+        this.$supers('bind_', arguments);
+
+        var editor = ace.edit("editor");
+        editor.setTheme("ace/theme/github");
+        editor.getSession().setMode("ace/mode/ftl");
+        editor.setShowPrintMargin(false);
+        editor.setDisplayIndentGuides(false);
+        editor.setSelectionStyle('text'); // instead of default "line"
+        // store configured instance
+        $('#editor').data('editor', editor);
+    };
+
+    // doesn't work
+    //this.listen({onClick: function() {
+    //}});
 }
